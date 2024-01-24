c63d9222284c0a0b669347616b2b66c99d6efb58
Maberry00 <maberry30@gmail.com>
2024-01-24 10:27:41 -0600

    Cars

--- Classes/Program.cs
+++ Classes/Program.cs
@@ -1,10 +1,32 @@
-﻿namespace Classes
+﻿using Microsoft.VisualBasic;
+
+namespace Classes
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Car myCar = new Car();
+            myCar.Make = "Nissan";
+            myCar.Model = "GTR";
+            myCar.Year = 2010;
+
+            var jeep = new Car()
+            {
+                Make = "Jeep",
+                Model = "SRT",
+                Year = 2022
+            };
+
+            var ford = new Car("Ford", "Mustang", 1965);
+
+            var carList = new List<Car>() { myCar, ford, jeep};
+
+            foreach (var car in carList) 
+            {
+                Console.WriteLine($"{car.Make} {car.Model} {car.Year}");
+            }
+
         }
     }
 }
